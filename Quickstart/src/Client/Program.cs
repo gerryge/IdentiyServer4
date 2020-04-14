d992c7cb1f81e05deabb691a1f6272ee80cc9568
Gerry Ge <99432281@qq.com>
2020-04-14 10:50:55 +0800

    change start profile

--- Quickstart/src/Client/Program.cs
+++ Quickstart/src/Client/Program.cs
@@ -10,49 +10,56 @@ namespace Client
     {
         static async Task Main(string[] args)
         {
-            // discover endpoints from metadata
-            var client = new HttpClient();
-            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
-            if (disco.IsError)
+            try
             {
-                Console.WriteLine(disco.Error);
-                return;
+                // discover endpoints from metadata
+                var client = new HttpClient();
+                var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+                if (disco.IsError)
+                {
+                    Console.WriteLine(disco.Error);
+                    return;
+                }
+
+                // request token
+                var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+                {
+                    Address = disco.TokenEndpoint,
+
+                    ClientId = "client",
+                    ClientSecret = "secret",
+                    Scope = "api1"
+                });
+
+                if (tokenResponse.IsError)
+                {
+                    Console.WriteLine(tokenResponse.Error);
+                    return;
+                }
+
+                Console.WriteLine(tokenResponse.Json);
+
+                // call api
+                var apiClient = new HttpClient();
+                apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+                var response = await apiClient.GetAsync("http://localhost:5001/identity");
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(response.StatusCode);
+                }
+                else
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(JArray.Parse(content));
+                }
+
+                Console.ReadKey();
             }
-
-            // request token
-            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            catch (Exception ex)
             {
-                Address = disco.TokenEndpoint,
-
-                ClientId = "client",
-                ClientSecret = "secret",
-                Scope = "api1"
-            });
-
-            if (tokenResponse.IsError)
-            {
-                Console.WriteLine(tokenResponse.Error);
-                return;
+                throw ex;
             }
-
-            Console.WriteLine(tokenResponse.Json);
-
-            // call api
-            var apiClient = new HttpClient();
-            apiClient.SetBearerToken(tokenResponse.AccessToken);
-
-            var response = await apiClient.GetAsync("http://localhost:5001/identity");
-            if (!response.IsSuccessStatusCode)
-            {
-                Console.WriteLine(response.StatusCode);
-            }
-            else
-            {
-                var content = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(JArray.Parse(content));
-            }
-
-            Console.ReadKey();
         }
     }
 }
