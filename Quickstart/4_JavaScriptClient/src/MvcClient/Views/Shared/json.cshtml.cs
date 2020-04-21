705810eb861d0cccd030508f96981d2bac3b45b9
Gerry Ge <99432281@qq.com>
2020-04-21 17:10:53 +0800

    Adding a JavaScript client

---
+++ Quickstart/4_JavaScriptClient/src/MvcClient/Views/Shared/json.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvcClient.Views.Shared
+{
+    public class jsonModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
