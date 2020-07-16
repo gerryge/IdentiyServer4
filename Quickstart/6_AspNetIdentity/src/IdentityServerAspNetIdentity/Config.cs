8c232de989bc3f3e9c8310b477f4b57b83b2f961
Gerry Ge <99432281@qq.com>
2020-07-16 11:31:54 +0800

    expanded version if more control is needed

--- Quickstart/6_AspNetIdentity/src/IdentityServerAspNetIdentity/Config.cs
+++ Quickstart/6_AspNetIdentity/src/IdentityServerAspNetIdentity/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityModel;
 using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
@@ -15,12 +16,47 @@ namespace IdentityServerAspNetIdentity
             {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
+                new IdentityResources.Email(),
             };
 
         public static IEnumerable<ApiResource> Apis =>
             new List<ApiResource>
             {
-                new ApiResource("api1","My API")
+                // simple API with a single scope (in this case the scope name is the same as the api name)
+                new ApiResource("api1","My API"),
+
+                #region expanded version	
+                // expanded version if more control is needed
+                /*
+                new ApiResource
+                {
+                    Name = "api2",
+
+                    // secret for using introspection endpoint
+                    ApiSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // include the following using claims in access token (in addition to subject id)
+                    UserClaims = { JwtClaimTypes.Name, JwtClaimTypes.Email },
+
+                    // this API defines two scopes
+                    Scopes =
+                    {
+                        new Scope()
+                        {
+                            Name = "api2.full_access",
+                            DisplayName = "Full access to API 2",
+                        },
+                        new Scope
+                        {
+                            Name = "api2.read_only",
+                            DisplayName = "Read only access to API 2"
+                        }
+                    }
+                }*/
+                #endregion
             };
 
         public static IEnumerable<Client> Clients =>
@@ -66,7 +102,7 @@ namespace IdentityServerAspNetIdentity
                         "api1"
                     },
 
-                    AllowOfflineAccess=true
+                    AllowOfflineAccess=true,
                 },
                 // JavaScript Client
                 new Client
